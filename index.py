cde259a6c91f2b319c9cb8a3909302eb5bf24022
Faiz Rabbani <faizrabbani.035@gmail.com>
2026-09-17 21:22:38 -0400

    Added proper statements to main, and test case to showcase results

--- index.py
+++ index.py
@@ -4,10 +4,6 @@ import os
 import requests
 from dotenv import load_dotenv
 
-query = input("Enter your search query: ")
-
-
-
 
 #loads up the api key
 def load_api_key():
@@ -21,5 +17,9 @@ def fetch_news(query, api_key):
     response = requests.get(url)
     return response.json()
 
-with open("news.json", "w") as f:
-    json.dump(fetch_news(query, load_api_key()), f, indent=4)
+if __name__ == "__main__":
+    query = input("Enter your search query: ")
+    with open("news.json", "w") as f:
+        json.dump(fetch_news(query, load_api_key()), f, indent=4)
+    data = fetch_news(query, load_api_key())
+    print(f'found {data["totalResults"]} total results ')
