8b171b310d00371f9d0e16270d93b963ab1f998c
Faiz Rabbani <faizrabbani.035@gmail.com>
2026-09-17 21:17:03 -0400

    Function calls added to make code modular for fetching news

--- index.py
+++ index.py
@@ -1,15 +1,25 @@
-from dotenv import load_dotenv
-import os
-import requests
 import json
+import os
 
-load_dotenv()
-
-api_key = os.getenv("API_KEY")
+import requests
+from dotenv import load_dotenv
 
 query = input("Enter your search query: ")
-url = f"https://newsdata.io/api/1/latest?apikey={api_key}&q={query}"
-response = requests.get(url)
 
-with open("output.json", "w") as f:
-    json.dump(response.json(), f)
+
+
+
+#loads up the api key
+def load_api_key():
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+    return api_key
+
+#fetches news based on query
+def fetch_news(query, api_key):
+    url = f"https://newsdata.io/api/1/latest?apikey={api_key}&q={query}"
+    response = requests.get(url)
+    return response.json()
+
+with open("news.json", "w") as f:
+    json.dump(fetch_news(query, load_api_key()), f, indent=4)
